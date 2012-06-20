c762f31694921b184ca89abb9e82bd812cc23685
Matt Rogish <matt.rogish@gmail.com>
2012-06-20 16:35:15 -0400

    Adding sublime text2, changing config for it

--- setup.rb
+++ setup.rb
@@ -5,10 +5,16 @@
 home = File.expand_path('~')
 
 Dir['*'].each do |file|
-  next if file =~ /setup/
+  next if file =~ /setup|Preferences/
   target = File.join(home, ".#{file}")
-  `ln -s #{File.expand_path file} #{target}`
+  # `ln -s #{File.expand_path file} #{target}`
 end
+
+# Sublime text2 profile is in a weird location
+sublime_file = 'Preferences.sublime-settings'
+sublime_path = File.join home, 'Library', 'Application\ Support', 'Sublime\ Text\ 2', 'Packages', 'User'
+`rm #{File.expand_path File.join(sublime_path, sublime_file)}`
+`ln -s #{File.expand_path sublime_file} #{sublime_path}`
 
 # git push on commit
 `echo 'git push' > .git/hooks/post-commit`
