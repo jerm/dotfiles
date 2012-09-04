4f9ca3b3b9caf2a9db9af676aff67df3cf46b492
Matt Rogish <matt.rogish@gmail.com>
2012-09-04 15:06:29 -0400

    Adding sublime bundles

--- setup.rb
+++ setup.rb
@@ -12,9 +12,21 @@ end
 
 # Sublime text2 profile is in a weird location
 sublime_file = 'Preferences.sublime-settings'
-sublime_path = File.join home, 'Library', 'Application\ Support', 'Sublime\ Text\ 2', 'Packages', 'User'
-`rm #{File.expand_path File.join(sublime_path, sublime_file)}`
-`ln -s #{File.expand_path sublime_file} #{sublime_path}`
+sublime_path = File.join home, 'Library', 'Application\ Support', 'Sublime\ Text\ 2', 'Packages'
+sublime_user_path = File.join sublime_path, 'User'
+`rm #{File.expand_path File.join(sublime_user_path, sublime_file)}`
+`ln -s #{File.expand_path sublime_file} #{sublime_user_path}`
+
+sublime_bundles = [ { :git_url     => "git://github.com/jashkenas/coffee-script-tmbundle",
+                      :bundle_name => "CoffeeScriptBundle.tmbundle" }
+  ]
+
+sublime_bundles.each do |bundle|
+  bundle_path = File.join sublime_path, bundle[:bundle_name]
+  `rm -rf #{bundle_path}`
+  `git clone #{bundle[:git_url]} #{bundle_path}`
+end
+
 
 # git push on commit
 `echo 'git push' > .git/hooks/post-commit`
