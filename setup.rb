ad707a57f99e93ff18192262a750dde29cc9cf79
Matt Rogish <MattRogish@users.noreply.github.com>
2013-10-07 16:16:04 -0400

    Update setup.rb

--- setup.rb
+++ setup.rb
@@ -17,16 +17,6 @@ sublime_user_path = File.join sublime_path, 'User'
 `rm #{File.expand_path File.join(sublime_user_path, sublime_file)}`
 `ln -s #{File.expand_path sublime_file} #{sublime_user_path}`
 
-sublime_bundles = [ { :git_url     => "git://github.com/jashkenas/coffee-script-tmbundle",
-                      :bundle_name => "CoffeeScriptBundle.tmbundle" }
-  ]
-
-sublime_bundles.each do |bundle|
-  bundle_path = File.join sublime_path, bundle[:bundle_name]
-  `rm -rf #{bundle_path}`
-  `git clone #{bundle[:git_url]} #{bundle_path}`
-end
-
 
 # git push on commit
 `echo 'git push' > .git/hooks/post-commit`
