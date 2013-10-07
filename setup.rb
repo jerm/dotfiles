0ce6740223f7dc2dd0073ce87a13e9433303ac70
Matt Rogish <MattRogish@users.noreply.github.com>
2013-10-07 16:15:37 -0400

    Update setup.rb

--- setup.rb
+++ setup.rb
@@ -12,7 +12,7 @@ end
 
 # Sublime text2 profile is in a weird location
 sublime_file = 'Preferences.sublime-settings'
-sublime_path = File.join home, 'Library', 'Application\ Support', 'Sublime\ Text\ 2', 'Packages'
+sublime_path = File.join home, 'Library', 'Application\ Support', 'Sublime\ Text\ 3', 'Packages'
 sublime_user_path = File.join sublime_path, 'User'
 `rm #{File.expand_path File.join(sublime_user_path, sublime_file)}`
 `ln -s #{File.expand_path sublime_file} #{sublime_user_path}`
